c58133af3f06f59e785991b1ac3253ac03fc0bfc
Eric Zhang <Eric.Zhang@okkuaixiu.com>
2016-03-04 15:46:39 +0800

    合并库文件到TeemoDemo项目，并删除库文件（便于修改和查看代码）

--- TeemoDemo/gen/com/teemo/demo/R.java
+++ TeemoDemo/gen/com/teemo/demo/R.java
@@ -17,9 +17,9 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040001;
-        public static final int activity_vertical_margin=0x7f040002;
-        public static final int dgv_overlap_if_switch_straight_line=0x7f040000;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+        public static final int dgv_overlap_if_switch_straight_line=0x7f040002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
