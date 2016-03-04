5bb789461a72c33f7e70c8237358a2fb6bff1900
Eric Zhang <Eric.Zhang@okkuaixiu.com>
2016-03-04 15:24:57 +0800

    修改可以拖动排序元素为9个

--- TeemoDemo/src/com/teemo/dynamic/gridview/Cheeses.java
+++ TeemoDemo/src/com/teemo/dynamic/gridview/Cheeses.java
@@ -1,35 +1,40 @@
 /*
-*****************************************************************************************
-* @file Cheeses.java
-*
-* @brief 
-*
-* Code History:
-*       2016-3-2  上午11:16:05  Teemo , initial version
-*
-* Code Review:
-*
-********************************************************************************************
-*/
+ *****************************************************************************************
+ * @file Cheeses.java
+ *
+ * @brief 
+ *
+ * Code History:
+ *       2016-3-2  上午11:16:05  Teemo , initial version
+ *
+ * Code Review:
+ *
+ ********************************************************************************************
+ */
 
 package com.teemo.dynamic.gridview;
+
 /**
- * @brief 
+ * @brief
  * 
  * @author Teemo
- *
+ * 
  * @date 2016-3-2 上午11:16:05
  */
 public class Cheeses {
-    public static final String[] sCheeseStrings = {
-        "Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
-        "Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu", "Airag", "Airedale",
-        "Aisy Cendre", "Allgauer Emmentaler", "Alverca", "Ambert", "American Cheese",
-        "Ami du Chambertin", "Anejo Enchilado", "Anneau du Vic-Bilh", "Anthoriro", "Appenzell",
-        "Aragon", "Ardi Gasna", "Ardrahan", "Armenian String", "Aromes au Gene de Marc",
-        "Asadero", "Asiago", "Aubisque Pyrenees", "Autun", "Avaxtskyr", "Baby Swiss",
-        "Babybel", "Baguette Laonnaise", "Bakers", "Baladi", "Balaton", "Bandal", "Banon",
-        "Barry's Bay Cheddar"
-};
+	public static final String[] sCheeseStrings = { "Abbaye de Belloc",
+			"Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
+			"Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu" };
+	// public static final String[] sCheeseStrings = { "Abbaye de Belloc",
+	// "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
+	// "Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu",
+	// "Airag", "Airedale", "Aisy Cendre", "Allgauer Emmentaler",
+	// "Alverca", "Ambert", "American Cheese", "Ami du Chambertin",
+	// "Anejo Enchilado", "Anneau du Vic-Bilh", "Anthoriro", "Appenzell",
+	// "Aragon", "Ardi Gasna", "Ardrahan", "Armenian String",
+	// "Aromes au Gene de Marc", "Asadero", "Asiago", "Aubisque Pyrenees",
+	// "Autun", "Avaxtskyr", "Baby Swiss", "Babybel",
+	// "Baguette Laonnaise", "Bakers", "Baladi", "Balaton", "Bandal",
+	// "Banon", "Barry's Bay Cheddar" };
 
 }
